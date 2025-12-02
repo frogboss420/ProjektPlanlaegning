71f50eb3825331dcc5ec627d6e7de8672873ddc2
frogboss420 <mads.friis.nielsen@gmail.com>
2025-12-02 10:22:53 +0100

    read write json ny init

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 from datetime import datetime
 
 class DatoBehandler:
@@ -33,15 +34,47 @@ class DatoBehandler:
 
 
 class Fil:
-    def __init__(self):
+    def __init__(self, name: str):
         db = DatoBehandler()
+        if not os.path.isfile("Filer/"+name+".json"):
+            self.deadline = db.deadlineInput()
+        else:
+            with open("Filer/"+name+".json", "r") as file:
+                finfo = json.load(file)
+                self.deadline = finfo["Deadline"]
+                file.close()
         self.dato = db.dato()
-        self.deadline = db.deadlineInput()
-        self.info = {"Dato":self.dato, "Deadline":self.deadline}
+        self.info = {"Dato":self.dato, "Deadline":self.deadline, "Navn":name}
 
-test = Fil()
+    def writeFile(self):
+        #potentiel overflødig kode
+        '''if not os.path.isfile("Filer/" + self.info["Navn"]):
+            self.info["Navn"]: str = input("Angiv filnavn\n")
+            print(self.info["Navn"])'''
+        with open("Filer/" + self.info["Navn"] + ".json", "w") as file:
+            json.dump(self.info, file)
+            file.close()
 
-jsontest = json.dumps(test.info) #konverter python dict til json string (s i dumps betyder str)
+
+    def readFile(self):
+        with open("Filer/" + self.info["Navn"] + ".json", "r") as file:
+            print(json.load(file))
+            file.close()
+
+
+
+test = Fil("test")
+#test.writeFile()
+test.readFile()
+
+
+'''jsontest = json.dumps(test.info) #konverter python dict til json string (s i dumps betyder str)
 
 loadjsontest = json.loads(jsontest) #konverter json string til python dict
 print("dato test " + str(loadjsontest["Dato"]) + "\ndeadline test " + str(loadjsontest["Deadline"]))
+
+with open("Filer/test.txt","w") as file:
+    file.write("test text")
+
+if os.path.isfile("Filer/test.txt"):
+    print("test")'''
